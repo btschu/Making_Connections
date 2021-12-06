4b8fd40170d8a2f45f9d3597a50ceba970492fbc
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-05 18:49:04 -0700

    cleaned up js

--- script.js
+++ script.js
@@ -10,18 +10,31 @@ function changeText() {
 
 // increase connections and decrease requests based off which button is clicked. Also removes the line where the button is located.
 
-var count = 2
-var remove1 = document.querySelector('.badge1')
-var add1 = document.querySelector('.badge')
-var connections = 489
-
-function hide(word, user1) {
-    var approveDeny=document.querySelector(user1)
-    approveDeny.remove();
-    count--;
-    remove1.innerHTML = count;
-    if(word.getAttribute("alt")=="accept"){
-        connections++;
-        add1.innerHTML = connections;
+function connectionRequests(element){
+    if(element.alt == "accept"){
+        document.querySelector('.badge').innerHTML++;
+        document.querySelector('.badge1').innerHTML--;
+    } else {
+        document.querySelector('.badge1').innerHTML--;
     }
+    element.parentElement.parentElement.remove();
 }
+
+
+// old solution
+
+// var count = 2
+// var remove1 = document.querySelector('.badge1')
+// var add1 = document.querySelector('.badge')
+// var connections = 489
+
+// function hide(element, user1) {
+//     var approveDeny=document.querySelector(user1)
+//     approveDeny.remove();
+//     count--;
+//     remove1.innerHTML = count;
+//     if(element.getAttribute("alt")=="accept"){
+//         connections++;
+//         add1.innerHTML = connections;
+//     }
+// }
